5a3d368d928ecc44db955671ce090c46f6c5cf67
Gustavobr20 <gustavo.henrique.oliveira50@gmail.com>
2024-10-16 20:32:04 -0300

    feat: adicionando historico ao robo com mongodb

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_mongodb import MongoDBChatMessageHistory
 
 load_dotenv(override=True)
 
@@ -24,6 +25,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+history = MongoDBChatMessageHistory('mongodb://root:password@localhost:27017/', '2')
+
 class ChatBody(BaseModel):
     message: str
 
@@ -31,16 +34,24 @@ class ChatBody(BaseModel):
 def chat(body: ChatBody):
     llm = ChatGroq(model='gemma2-9b-it', max_tokens=1024)
 
-    prompt = [
-        ('system', """
-            You are a greatest AI assistant in the world. 
-            You must identify in which language this message is being sent and respond in the same language.
-            You not must inform the language you are using.
-            You must only use the language that the user is using.
-         """),
-        ('human', body.message),
-    ]
-    return llm.invoke(prompt)
+    print(history.messages)
+
+    history.add_user_message(body.message)
+
+    # prompt = [
+    #     ('system', """
+    #         You are a greatest AI assistant in the world. 
+    #         You must identify in which language this message is being sent and respond in the same language.
+    #         You not must inform the language you are using.
+    #         You must only use the language that the user is using.
+    #      """),
+    #     ('human', body.message),
+    # ]
+    response = llm.invoke([message.content for message in history.messages])
+
+    history.add_ai_message(response.content)
+
+    return response
 
 if __name__ == '__main__':
     uvicorn.run('__main__:app', host=API_HOST, port=int(API_PORT), reload=True)
